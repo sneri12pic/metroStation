737e4503b9748d310a0a100da3710137939986bc
Stepan Demianenko <42498337+sneri12pic@users.noreply.github.com>
2023-10-29 14:00:56 +0000

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace coursework1_OOSD
+{
+    public partial class MainWindow : Window
+    {
+        //Console creation -->
+        [DllImport("Kernel32")]
+        public static extern void AllocConsole();
+        // -- end
+
+        MetroStation metro = new MetroStation();
+        public MainWindow()
+        {
+            InitializeComponent();
+            AllocConsole();
+            try
+            {
+                Console.WriteLine("Type ID : ");
+                metro.ID = Console.ReadLine();
+                Console.WriteLine("Type Name : ");
+                metro.Name = Console.ReadLine();
+                Console.WriteLine("Type in Line : ");
+                metro.Line = Console.ReadLine();
+                metro.sellTicket();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("ID is : " + metro.ID);
+            Console.WriteLine("Name is : " + metro.Name);
+            Console.WriteLine("\nTickets sold: " + metro.TicketSold);
+            //Console.WriteLine("ID " + metro.ID);
+
+        }
+
+        private void txtbTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtbMainTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtbMainTextBox.Text = "Do you want to add a Station? : ";
+        }
+
+        private void btnMain_yes_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnMain_no_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MetroStation.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace coursework1_OOSD
+{
+    public class MetroStation
+    {
+        private String _id;
+      
+        public string ID   // property
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                int id_num = 0;
+                if (_id != null)
+                {
+                    id_num = Int32.Parse(_id);
+                }
+                else if (value == "")
+                {
+                    throw new ArgumentException("ID cannot be blank!", "ID");
+                }
+                else if (id_num <= 999 && id_num >= 2001)
+                {
+                    throw new ArgumentException("ID cannot is out of bound!", "ID");
+                }
+                /*_id = id_num.ToString();*/
+                _id = value;
+            }
+        }
+
+        // Name of the Station creation
+        private string name;
+        public string Name
+        {
+            // Getter name
+            get { return name; }
+            // Setter name
+            set
+            {
+                // Checking so the line is not blank
+                if (value == "")
+                {
+                    throw new ArgumentException("Error Name is blank! ", "Name");
+
+                }
+                name = value;
+                
+            }
+        }
+
+        // Station Line creation
+        private string line;
+        public string Line
+        {
+            //getter, setter for 'Line'
+            get { return line; }
+            set
+            {
+                // Checking so the line is not blank
+                if (value == "")
+                {
+                    throw new ArgumentException("Error Line is blank! ", "Line");
+                }
+                line = value;
+            }
+        }
+
+        private int ticketsSold = 0;
+        // Making another ticket sold to manipulate the actual value publicly
+        public int TicketSold
+        {
+            // Getter Tickets Sold
+            get { return ticketsSold; }
+        }
+
+        //Sell Tickets Method
+        public void sellTicket()
+        {
+            // Keeping tickets under set boundaries of 999999
+            if(ticketsSold <= 999999)
+            {
+                ticketsSold++;
+            }
+            else
+            {
+                throw new ArgumentException("Error number of tickets went over the set limit ", "TicketSold");
+            }
+        }
+    }
+}
